ccb5e57060e0361cfaa09721d2c68360ca869c8a
Christoph <christoph@ag-prop.com>
2021-12-22 17:05:02 +0100

    Feat(ConnectionList): Add sendMessage

--- app/components/setup/connection-list/index.ts
+++ app/components/setup/connection-list/index.ts
@@ -1,5 +1,8 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { DataConnection } from 'peerjs';
 import PeerService from 'flimmerkasten-host/services/peer';
 
 interface Args {
@@ -8,4 +11,15 @@ interface Args {
 export default class SetupComponent extends Component <Args> {
   // Services
   @service peer!: PeerService;
+
+
+  // Defaults
+  @tracked value: string = '';
+
+
+  // Actions
+  @action
+  sendMessage(connection: DataConnection) {
+    connection.send(this.value);
+  }
 }
