d917d99618a052d12bab32bcd6ec172649e7c616
Christoph <christoph@ag-prop.com>
2021-12-22 17:03:52 +0100

    Refactor(UiButton): Use yield for label and remove onClick action

--- app/components/ui/button/index.ts
+++ app/components/ui/button/index.ts
@@ -1,9 +1,7 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 
 interface Args {
   disabled: boolean,
-  onClick: Function
   style: string
   type: string
 }
@@ -12,14 +10,5 @@ export default class UiButtonComponent extends Component<Args> {
   // Getter and setter
   get type() {
     return this.args.type ?? 'button';
-  }
-
-
-  // Actions
-  @action
-  onClick() {
-    if (this.args.onClick && !this.args.disabled) {
-      this.args.onClick();
-    }
   }
 }
