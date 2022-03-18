1baaab028a3137edcf3c3c15cf0c03504b67436e
Christoph Wiedenmann <christoph@ag-prop.com>
2022-03-18 10:28:06 +0100

    Clean(EmberAnimated): Remoce ember animated

--- app/components/application/confirm/index.ts
+++ app/components/application/confirm/index.ts
@@ -3,12 +3,6 @@ import { inject as service } from '@ember/service';
 import Confirm from 'flimmerkasten-host/services/confirm';
 // @ts-ignore
 import { keyResponder, onKey } from 'ember-keyboard';
-// @ts-ignore
-import move from 'ember-animated/motions/move';
-// @ts-ignore
-import TransitionContext from 'ember-animated/-private/transition-context';
-// @ts-ignore
-import { easeOut, easeIn } from 'ember-animated/easings/cosine';
 
 @keyResponder
 export default class ApplicationConfirm extends Component {
@@ -31,24 +25,5 @@ export default class ApplicationConfirm extends Component {
   cancelOnEscape(event: Event) {
     this.confirm.cancel();
     event.preventDefault();
-  }
-
-
-  // Functions
-  *slideFromBottom(
-    {
-      insertedSprites,
-      removedSprites
-    }: TransitionContext
-  ) {
-    for (let sprite of removedSprites) {
-      sprite.endAtPixel({ y: window.innerHeight });
-      move(sprite, { easing: easeIn });
-    }
-
-    for (let sprite of insertedSprites) {
-      sprite.startAtPixel({ y: window.innerHeight });
-      move(sprite, { easing: easeOut });
-    }
   }
 }
--- app/components/application/index.ts
+++ app/components/application/index.ts
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import FlashService from 'ember-cli-flash/services/flash-messages';
-import ENV from 'flimmerkasten-host/config/environment';
 
 export default class ApplicationComponent extends Component {
   // Services
@@ -10,10 +9,4 @@ export default class ApplicationComponent extends Component {
 
   // Defaults
   blockName = 'c-application';
-
-
-  // Getter and Setter
-  get isDevelopment() {
-    return ENV.environment === 'development';
-  }
 }
