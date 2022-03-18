70aca371c0334302b16668b1a30d47b396b262f9
Christoph Wiedenmann <christoph@ag-prop.com>
2022-03-18 15:05:58 +0100

    Fix(Bem): Fix typescript error

--- app/helpers/bem.ts
+++ app/helpers/bem.ts
@@ -2,13 +2,13 @@ import { helper } from '@ember/component/helper';
 import { isEmpty } from '@ember/utils';
 import { isArray } from '@ember/array';
 
-interface Modifiers {
-  modifiers: {
-    [key: string]: string
-  }
-}
+// interface Modifiers {
+//   modifiers: {
+//     [key: string]: string
+//   }
+// }
 
-export function bem(params: [string, string], { modifiers }: Modifiers) {
+export function bem(params: [string, string], { modifiers }: any) {
   let [block, element] = params;
   let classes: string[] = [];
   let baseClass = !isEmpty(element) ? `${block}__${element}` : block;
