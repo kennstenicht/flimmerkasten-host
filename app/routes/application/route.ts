ab51646a90fa82540b11e78638d0c1bdb7dbfcc9
Christoph Wiedenmann <christoph@ag-prop.com>
2022-03-18 10:28:23 +0100

    Clean(HeadData): Remoce head data service

--- app/routes/application/route.ts
+++ app/routes/application/route.ts
@@ -1,11 +1,9 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import IntlService from 'ember-intl/services/intl';
-import HeadDataService from 'flimmerkasten-host/services/head-data';
 
 export default class ApplicationRoute extends Route {
   // Services
-  @service headData!: HeadDataService;
   @service intl!: IntlService;
 
 
@@ -13,13 +11,5 @@ export default class ApplicationRoute extends Route {
   beforeModel() {
     // Set Locale
     this.intl.setLocale('de');
-
-    // Setup head data fallback tags
-    this.headData.fallbackMetaTags = {
-      title: this.intl.t('application.meta.title'),
-      description: this.intl.t('application.meta.description'),
-      type: 'website',
-      structuredData: null,
-    }
   }
 }
