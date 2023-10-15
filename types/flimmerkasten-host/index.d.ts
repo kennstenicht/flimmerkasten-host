fd9c66f4c777aa4f3c1468e6cfa3347f032595e0
Christoph Wiedenmann <christoph@ag-prop.com>
2023-10-15 09:08:49 +0200

    Feat(Types): Use native ember types

--- types/flimmerkasten-host/index.d.ts
+++ types/flimmerkasten-host/index.d.ts
@@ -1,14 +1,5 @@
-import RouteInfo from '@ember/routing/-private/route-info';
+import 'ember-source/types';
 
-declare global {
-  // interface Array<T> extends ArrayPrototypeExtensions<T> {}
-  // interface Function extends Ember.FunctionPrototypeExtensions {}
-
-  interface CustomRouteInfo extends RouteInfo {
-    metadata: {
-      metaTags: Function
-    }
-  }
-}
+declare global {}
 
 export {};
